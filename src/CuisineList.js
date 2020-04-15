c8657fc8fcff3543054148e9c6be4ebb896fe779
Jens Duray <61234904+Bobaflex-HSV@users.noreply.github.com>
2020-04-15 23:16:28 +0200

    change/add of components
    + Category Card
    + Cuisine Card

--- src/CuisineList.js
+++ src/CuisineList.js
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
+import CuisineCard from "./CuisineCard";
 import "./styles.css";
 
 export default () => {
-  return <div> PLACEHOLDER FOR ADDING CUSINE(AREA/COUNTRY) </div>;
+  return (
+    <div>
+      <h1> PLACEHOLDER FOR CUSINE(AREA/COUNTRY) </h1>
+      <div>
+        {/* iterate over cuisine(area) here*/}
+        <CuisineCard />
+      </div>
+    </div>
+  );
 };
