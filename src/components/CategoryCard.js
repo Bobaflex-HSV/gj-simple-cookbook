c53226c1b7f07a0b28af5302409712550486736f
Jens Duray <61234904+Bobaflex-HSV@users.noreply.github.com>
2020-04-18 19:07:31 +0200

    test CategoryCard

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import "../categorycard.scss";
 import "/src/styles.css";
 
-export default ({category}) => {
+export default ({ category }) => {
   return (
     <div>
-      <div>
-      {category}
+      <div>{category}</div>
+      <div class="card">
+        <div class="header" />
+        <a href="#" className="btn">
+          {category}
+        </a>
       </div>
     </div>
   );
