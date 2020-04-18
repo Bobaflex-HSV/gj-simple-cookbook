035874a830b37fba05b857b23c6dafe421b40c00
Jens Duray <61234904+Bobaflex-HSV@users.noreply.github.com>
2020-04-18 16:41:20 +0200

    add RecipeCard test on AddRecipe

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import RecipeCard from "./RecipeCard";
 import "/src/styles.css";
 
 export default () => {
-  return <div> PLACEHOLDER FOR ADDING RECIPES </div>;
+  return (
+    <div>
+      PLACEHOLDER FOR ADDING RECIPES
+      <RecipeCard />
+    </div>
+  );
 };
---
+++ src/components/RecipeCard.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "../recipecard.scss";
+import "/src/styles.css";
+
+export default () => {
+  return (
+    <div>
+      <div class="card">
+        <div class="header" />
+        <div class="text">
+          <h1 class="food">Chinese Noodles</h1>
+          <i class="fa fa-clock-o"> Chinese</i>
+          <i class="fa fa-users"> Vegetables</i>
+
+          <p class="info">Maybe some text...or maybe not</p>
+        </div>
+        <a href="#" class="btn">
+          Let's Cook!
+        </a>
+      </div>
+    </div>
+  );
+};
