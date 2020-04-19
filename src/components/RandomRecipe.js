8f55b6a351885b19ebe6c6c743c062325f4074d4
Gregfanyan <57916406+Gregfanyan@users.noreply.github.com>
2020-04-19 17:05:50 +0200

    random recipe is set
    random recipe is set

--- src/components/RandomRecipe.js
+++ src/components/RandomRecipe.js
@@ -1,6 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CategoryCard from "./CategoryCard";
 import "/src/styles.css";
 
 export default () => {
-  return <div> PLACEHOLDER FOR DISPLAYING A RANDOM RECIPE </div>;
+  const [data, setData] = useState("");
+
+  const fetchData = () => {
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+      .then(res => res.json())
+      .then(result => setData(result.meals))
+      .catch(error => console.log("error"));
+  };
+
+  useEffect(() => {
+    fetchData();
+    setInterval(fetchData, 2000);
+  }, []);
+
+  return (
+    <div>
+      {data &&
+        data.map((element, index) => (
+          <CategoryCard
+            key={index}
+            imageURL={element.strCategoryThumb}
+            category={element.strMeal}
+            name={element.strCategory}
+          />
+        ))}
+    </div>
+  );
 };
