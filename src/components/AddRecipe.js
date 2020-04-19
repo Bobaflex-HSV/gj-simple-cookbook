c95b826ffcd66da3c78df150f499e4a95442fe27
Jens Duray <61234904+Bobaflex-HSV@users.noreply.github.com>
2020-04-19 17:42:30 +0200

    add placeholder component

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import RecipeCard from "./RecipeCard";
 import RecipeCardSmall from "./RecipeCardSmall";
+import Placeholder from "./Placeholder";
 import "/src/styles.css";
 
 export default () => {
   return (
     <div>
       PLACEHOLDER FOR ADDING RECIPES
+      <Placeholder />
       <RecipeCard />
       <RecipeCardSmall />
     </div>
