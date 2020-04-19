a73a26f09d88b475676aadcee343553bde9b29ad
Gregfanyan <57916406+Gregfanyan@users.noreply.github.com>
2020-04-19 17:06:57 +0200

    random recipe is set
    random recipe is set

---
+++ src/components/RandomRecipeCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "../categorycard.scss";
+import "/src/styles.css";
+
+export default ({ category, imageURL, catDescription }) => {
+  return (
+    <div>
+      {/* <img src={imageURL} alt={catDescription} /> */}
+
+      <div className="cc-card">
+        <div
+          className="header"
+          style={{
+            backgroundImage: `url(${imageURL})`
+          }}
+        />
+        <div className="text">
+          <h1 className="food">{category}</h1>
+          <p className="info">{catDescription}</p>
+        </div>
+        <a href="#" className="btn">
+          Cook {category}
+        </a>
+      </div>
+    </div>
+  );
+};
