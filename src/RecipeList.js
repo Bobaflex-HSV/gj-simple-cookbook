b02b3e18bc13ba60effb1192262c2b1bc36c1d79
Gregfanyan <57916406+Gregfanyan@users.noreply.github.com>
2020-04-29 14:06:07 +0200

    onSubmit button works
    onSubmit button listens to input events

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from "react";
 import Recipe from "./Recipe";
 import "./styles.css";
+import "./topmenu.css";
+
 
 export default function RecipeList() {
   const apiURL = "https://www.themealdb.com/api/json/v1/1/search.php?f=c";
-  const [myRecipes, setRecipes] = useState({meals: []});
+  const [myRecipes, setRecipes] = useState({ meals: [] });
   const [search, setSearch] = useState("");
+  const [filterRecipes, setFilterRecipes] = useState([]);
 
   // fetch recipe from API
   function fetchRecipes() {
     fetch(apiURL)
       .then(response => response.json())
-      .then(data => setRecipes(data.meals))
+      .then(data => {
+        setRecipes(data.meals);
+        setFilterRecipes(data.meals);
+      })
       .catch(console.log("Error"));
   }
 
@@ -20,31 +26,27 @@ export default function RecipeList() {
       myRecipes.filter((element, filterIndex) => index !== filterIndex)
     );
   }
+
   useEffect(() => {
     fetchRecipes();
   }, []);
 
-  const filterRecipes = myRecipes.length 
-  ?  myRecipes.filter(element => {
-     return element.strMeal.toLowerCase().includes(search.toLocaleLowerCase());
-   })
-  : [];
+  const searching = e => {
+    setSearch(e.target.value);
+  };
 
-  const searching = event => {
-    setSearch(event.target.value)
-    }
-
-    const mySearch = event => {
-      event.preventDefault();
-      setRecipes(search);
-      setSearch("");
-    }
+  const getSearch = e => {
+    e.preventDefault();
+    const newFilterRecipes = myRecipes.filter(element => {
+      return element.strMeal.toLowerCase().includes(search.toLocaleLowerCase());
+    });
+    setFilterRecipes(newFilterRecipes);
+    setSearch("");
+  };
 
   return (
     <div>
-      <label>
-      <form  onSubmit = {mySearch} className = "search-form">
-
+      <form onSubmit={getSearch} className="search-form">
         <div className="input-group mb-3 cb-search">
           <input
             type="text"
@@ -53,22 +55,20 @@ export default function RecipeList() {
             aria-label="Recipient's username"
             aria-describedby="button-addon2"
             onChange={searching}
-            value = {search}
+            value={search}
           />
           <div className="input-group-append">
             <button
               className="btn btn-outline-secondary"
-              type="button"
+              type="submit"
               id="button-addon2"
             >
               Search
             </button>
-
           </div>
         </div>
-        </form>
+      </form>
 
-      </label>
       <div>
         <button
           className="btn btn-info cb-button fetch-button"
@@ -78,14 +78,13 @@ export default function RecipeList() {
         </button>
         <br />
         {filterRecipes.map((element, index) => (
-            <Recipe
-              key={index}
-              index={index}
-              onDelete={onDeleteHandler}
-              {...element}
-              name={element.strMeal}
-            />
-          ))}
+          <Recipe
+            key={index}
+            index={index}
+            onDelete={onDeleteHandler}
+            {...element}
+          />
+        ))}
         {/** name of child component */}
         {/** strMeal is the name of Recipe in API object */}
       </div>
